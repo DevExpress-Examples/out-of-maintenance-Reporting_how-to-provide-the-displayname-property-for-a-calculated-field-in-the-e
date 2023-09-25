021841641ebf944ceceb794f65efd8ac213f5233
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-25 22:40:38 +0400

    Change example status to out of maintenance

--- CS/ChangeDisplayNameForCalculatedField/XtraReport1.cs
+++
@@ -1,8 +0,0 @@
-namespace ChangeDisplayNameForCalculatedField {
-    public partial class XtraReport1 : DevExpress.XtraReports.UI.XtraReport {
-        public XtraReport1() {
-            InitializeComponent();
-        }
-
-    }
-}
